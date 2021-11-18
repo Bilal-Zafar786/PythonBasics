06c282d2555917a40d789b0b018a9ecfba678024
Bilal-Zafar786 <bzcode786@gmail.com>
2021-11-18 19:32:02 +0500

    1st Python Basics Program

---
+++ base1.py
@@ -0,0 +1,139 @@
+# len(), str(), type()
+
+paragraph = "This is Ali's Laptop"
+
+# concatenation
+first = "Bilal"
+last = "Zafar"
+full = first+" "+last
+# print("My full name is: ",full)
+strinNumber = "1"
+# intNumber = float(strinNumber)
+# print(type(intNumber))
+# print(intNumber)
+# print(type(space))
+
+# List are Mutable it means you can change your list and List is Not Fix
+arr = [1,2,3,4,5]
+# print(arr)
+# print(type(arr))
+# print(type(arr[3]))
+# print(arr[5])
+# print(len(arr))
+
+# Slicing
+# list[start_index:end_index]
+# print(arr[:-2])
+
+# for i in range(0,10,2):
+#     print("Value of i is ",i)
+# arr = []
+# for i in range(11):
+#     arr.append(i)
+# print(arr)
+# arr = [i for i in range(15)] # List Comprehension
+# print(arr)
+# t = (1,2,3,4,5)
+# print(type(t))
+
+# Tuples within a List
+# Tuple_list = [(1,2,3,4),('a','b','c','d')]
+# print(Tuple_list[0])
+# List_Within_Tuple
+# tu = ([1,2,3],[4,5,6])
+# print(tu[0][2])
+
+
+# value = input("Enter your Value")
+# print(type(value))
+#
+# if value%2==0:
+#     print("Even NUMBER")
+# elif vas:
+#     asas
+#
+# else:
+#     print("ODD NUMBER")
+# num = int(input("Enter Value"))
+# if num>2: print('greater')
+# else: print("Not greater")
+
+# program to display student's marks from record
+# student_name = 'Arthur'
+#
+# marks = {'James': 90, 'Jules': 55, 'Arthur': 77}
+#
+# for student in marks:
+#     if student == student_name:
+#         # print(marks[student])
+#         print(student)
+#         break
+# else:
+#     print('No entry with that name found.')
+# text = "Enter your Number"
+# Number = int(input(text))
+# if Number >=90:
+#     print("Grade A")
+#
+# elif Number ==70 and Number <80:
+#     print("Grade B")
+#
+# else:
+#     print("Fail")
+
+# val = int(input("Enter your number"))
+# if val > 10:print("Number is greater than 10");print("IF is running")
+# else:print("Number is NOT greater than 10")
+
+# studentData = {'student1':['Omer','Khan',25],'student2':['Khan','Age',25]}
+# print(type(studentData))
+
+# a = int(input("ENter A value"))
+# b = int(input("ENter B value"))
+
+
+# Functions without Parameters
+# def sum():
+#     return a+b
+#
+# result = sum()
+# print("Result is: ",result)
+
+# Functions with Parameters
+# def sum(a,b):
+#     return a+b
+#
+# result = sum(40,15)
+# print("Result is: ",result)
+# Break and Continue
+# for i in range(15):
+    # if i != 5:
+        # print(f"{i} is: Hello world")
+    # else:
+        # continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
